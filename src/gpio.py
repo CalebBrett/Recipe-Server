324a42a753d210a39513e33a26f4b2bf4c301d08
CalebBrett <calebbrett44@gmail.com>
2023-12-15 02:13:12 -0400

    Fix input polarity

--- src/gpio.py
+++ src/gpio.py
@@ -46,23 +46,23 @@ def SetDisplay(DisplayKind,DisplayOn):
 while True:
     time.sleep(0.1)
 
-    if GPIO.input(ButtonRight)==1 and time.time()-KeypressButton>KeyCooldown:
+    if GPIO.input(ButtonRight)==0 and time.time()-KeypressButton>KeyCooldown:
         KeypressButton=time.time()
         print("RIGHT")
     
-    if GPIO.input(ButtonLeft)==1 and time.time()-KeypressButton>KeyCooldown:
+    if GPIO.input(ButtonLeft)==0 and time.time()-KeypressButton>KeyCooldown:
         KeypressButton=time.time()
         print("Left")
 
-    if GPIO.input(ButtonR)==1 and time.time()-KeypressButton>KeyCooldown:
+    if GPIO.input(ButtonR)==0 and time.time()-KeypressButton>KeyCooldown:
         KeypressButton=time.time()
         print("R")
 
-    if GPIO.input(ButtonL)==1 and time.time()-KeypressButton>KeyCooldown:
+    if GPIO.input(ButtonL)==0 and time.time()-KeypressButton>KeyCooldown:
         KeypressButton=time.time()
         print("L")
 
-    if GPIO.input(ButtonPower)==1 and time.time()-KeypressButton>KeyCooldown:
+    if GPIO.input(ButtonPower)==0 and time.time()-KeypressButton>KeyCooldown:
         KeypressButton=time.time()
         if DisplayOn==True:
             DisplayOn=False
